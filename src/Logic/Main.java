fb58397d1be8357d084a3126681a0b503c77a6ef
Jose <jmejiamu@gmail.com>
2021-06-06 23:51:08 -0700

    delete main from gui layer

--- src/Logic/Main.java
+++ src/Logic/Main.java
@@ -1,11 +1,21 @@
 
 package Logic;
 
+import GUI.MainWindow;
+
 
 public class Main {
     
     public static void main(String[] args) {
         
+        //Creation of the main window object
+        MainWindow mainWindow =  new MainWindow();
+        
+        //Display window 
+        mainWindow.setVisible(true);
+        
+         //Center window
+        mainWindow.setLocationRelativeTo(null);
     }
     
 }
