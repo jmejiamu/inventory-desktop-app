59aa1ffdf98ffbb9354b29e30547bab03d63c915
Jose <jmejiamu@gmail.com>
2021-06-07 16:46:13 -0700

    added anotation mapping

--- src/Logic/Inventory.java
+++ src/Logic/Inventory.java
@@ -1,14 +1,87 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Logic;
 
-/**
- *
- * @author joch_
- */
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 public class Inventory {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Basic
+    private String item_name;
+    private int item_num;
+    private int item_code;
+    private String item_type;
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    public Inventory() {
+    }
+
+    public Inventory(Long id, String item_name, int item_num, int item_code, Date date, String item_type) {
+        this.id = id;
+        this.item_name = item_name;
+        this.item_num = item_num;
+        this.item_code = item_code;
+        this.date = date;
+        this.item_type = item_type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getItem_name() {
+        return item_name;
+    }
+
+    public void setItem_name(String item_name) {
+        this.item_name = item_name;
+    }
+
+    public int getItem_num() {
+        return item_num;
+    }
+
+    public void setItem_num(int item_num) {
+        this.item_num = item_num;
+    }
+
+    public int getItem_code() {
+        return item_code;
+    }
+
+    public void setItem_code(int item_code) {
+        this.item_code = item_code;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getItem_type() {
+        return item_type;
+    }
+
+    public void setItem_type(String item_type) {
+        this.item_type = item_type;
+    }
+    
+    
     
 }
