bec943cdb3e72831a4cdd5596d76ec0f22d488de
Jose <jmejiamu@gmail.com>
2021-06-10 20:13:53 -0700

    added global object

--- src/Logic/InventoryController.java
+++ src/Logic/InventoryController.java
@@ -1,4 +1,3 @@
-
 package Logic;
 
 import Persistence.PersistenceController;
@@ -7,27 +6,24 @@ import java.util.List;
 
 public class InventoryController {
 
+    PersistenceController con = new PersistenceController();
+
     public void createData(String itemName, int item_num, int itemCode, String itemType, Date date) {
         Inventory inventory = new Inventory(itemName, item_num, itemCode, date, itemType);
-        
-        PersistenceController con =  new PersistenceController();
+        // Use Set instead
         con.createInventory(inventory);
-        
-        
-        
-        
+
     }
+
     // fix this
     public void getAllItem() {
-        PersistenceController con =  new PersistenceController();
+        PersistenceController con = new PersistenceController();
         List<Inventory> inventoryList = con.getAllData();
-        for(Inventory in : inventoryList){
+        for (Inventory in : inventoryList) {
             in.getItem_name();
             System.out.println("<><><" + in.getItem_name());
         }
-        
+
     }
-    
-    
-    
+
 }
