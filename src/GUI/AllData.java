8e6240d2d0350e26ef89fa0b9b0a5f80b4ce145d
Jose <jmejiamu@gmail.com>
2021-06-12 14:01:28 -0700

    added combobox validation\

--- src/GUI/AllData.java
+++ src/GUI/AllData.java
@@ -22,10 +22,12 @@ public class AllData extends javax.swing.JFrame {
      * Creates new form AllData
      */
     InventoryController inventoryController;
+    PersistenceController con;
 
     public AllData() {
         initComponents();
         inventoryController = new InventoryController();
+        con = new PersistenceController();
     }
 
     /**
@@ -54,9 +56,11 @@ public class AllData extends javax.swing.JFrame {
         codeInput = new javax.swing.JTextField();
         nameInput = new javax.swing.JTextField();
         numOfItemInput = new javax.swing.JTextField();
-        typeInput = new javax.swing.JTextField();
-        jButton3 = new javax.swing.JButton();
-        jButton4 = new javax.swing.JButton();
+        bntUpdate = new javax.swing.JButton();
+        btnDelete = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        idInput = new javax.swing.JTextField();
+        typeInput = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -105,9 +109,24 @@ public class AllData extends javax.swing.JFrame {
 
         jLabel7.setText("Type: ");
 
-        jButton3.setText("UPDATE");
+        nameInput.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                nameInputActionPerformed(evt);
+            }
+        });
 
-        jButton4.setText("DELETE");
+        bntUpdate.setText("UPDATE");
+        bntUpdate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                bntUpdateActionPerformed(evt);
+            }
+        });
+
+        btnDelete.setText("DELETE");
+
+        jLabel2.setText("Id:");
+
+        typeInput.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "-", "Computers", "Shoes", "Phones", "Cloths" }));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -133,19 +152,21 @@ public class AllData extends javax.swing.JFrame {
                     .addComponent(jLabel6)
                     .addComponent(jLabel3)
                     .addComponent(jLabel5)
-                    .addComponent(jLabel7))
+                    .addComponent(jLabel7)
+                    .addComponent(jLabel2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(typeInput)
                     .addComponent(stackedDate, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(codeInput)
                     .addComponent(nameInput)
-                    .addComponent(numOfItemInput, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 221, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(numOfItemInput, javax.swing.GroupLayout.DEFAULT_SIZE, 221, Short.MAX_VALUE)
+                    .addComponent(idInput)
+                    .addComponent(typeInput, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(bntUpdate, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(btnDelete, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(71, 71, 71))
         );
         jPanel1Layout.setVerticalGroup(
@@ -155,38 +176,36 @@ public class AllData extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jButton1)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(idInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(4, 4, 4)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jLabel4)
-                            .addComponent(stackedDate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(19, 19, 19)
-                        .addComponent(jButton2)))
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(16, 16, 16)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel6)
-                            .addComponent(codeInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(jLabel3)
-                            .addComponent(nameInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(32, 32, 32)
-                        .addComponent(jButton3)))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 39, Short.MAX_VALUE)
+                        .addComponent(jButton2)
+                        .addGap(4, 4, 4)
+                        .addComponent(jLabel4))
+                    .addComponent(stackedDate, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(bntUpdate)
+                    .addComponent(codeInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel6))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel3)
+                    .addComponent(nameInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel5)
                     .addComponent(numOfItemInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton4))
-                .addGap(28, 28, 28)
+                    .addComponent(btnDelete))
+                .addGap(32, 32, 32)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel7)
                     .addComponent(typeInput, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(34, 34, 34)
+                .addGap(28, 28, 28)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(30, 30, 30))
         );
@@ -201,7 +220,7 @@ public class AllData extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 13, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
@@ -223,7 +242,7 @@ public class AllData extends javax.swing.JFrame {
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-        PersistenceController con = new PersistenceController();
+
         List<Inventory> inventoryList = con.getAllData();
 
         for (Inventory in : inventoryList) {
@@ -233,15 +252,13 @@ public class AllData extends javax.swing.JFrame {
             String itemName = in.getItem_name();
             String itemNum = String.valueOf(in.getItem_num());
             String itemType = in.getItem_type();
-            
-            String [] tbData = {id,itemDate,code,itemName,itemNum,itemType};
-            
-            DefaultTableModel defaultTableModel = (DefaultTableModel)jTable1.getModel();
-            
-            defaultTableModel.addRow(tbData);
-            
 
-//              details.setText(in.getItem_name());
+            String[] tbData = {id, itemDate, code, itemName, itemNum, itemType};
+
+            DefaultTableModel defaultTableModel = (DefaultTableModel) jTable1.getModel();
+
+            defaultTableModel.addRow(tbData);
+
         }
     }//GEN-LAST:event_jButton2ActionPerformed
 
@@ -249,23 +266,52 @@ public class AllData extends javax.swing.JFrame {
     private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
         // TODO add your handling code here:
         DefaultTableModel defaultTableModel = (DefaultTableModel) jTable1.getModel();
-        
+
         int selectedRowIndex = jTable1.getSelectedRow();
+        idInput.setText(defaultTableModel.getValueAt(selectedRowIndex, 0).toString());
         codeInput.setText(defaultTableModel.getValueAt(selectedRowIndex, 2).toString());
         nameInput.setText(defaultTableModel.getValueAt(selectedRowIndex, 3).toString());
         numOfItemInput.setText(defaultTableModel.getValueAt(selectedRowIndex, 4).toString());
-        typeInput.setText(defaultTableModel.getValueAt(selectedRowIndex, 5).toString());
-        
+
+        if (defaultTableModel.getValueAt(selectedRowIndex, 5).toString().equals("Computers")) {
+            typeInput.setSelectedIndex(1);
+        } else if (defaultTableModel.getValueAt(selectedRowIndex, 5).toString().equals("Shoes")) {
+            typeInput.setSelectedIndex(2);
+        } else if (defaultTableModel.getValueAt(selectedRowIndex, 5).toString().equals("Phones")) {
+            typeInput.setSelectedIndex(3);
+        } else if (defaultTableModel.getValueAt(selectedRowIndex, 5).toString().equals("Cloths")) {
+            typeInput.setSelectedIndex(4);
+        }
+
     }//GEN-LAST:event_jTable1MouseClicked
+
+    private void bntUpdateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bntUpdateActionPerformed
+        // TODO add your handling code here:
+        int id = Integer.parseInt(idInput.getText());
+        Date itemDate = new Date();
+        int code = Integer.parseInt(codeInput.getText());
+        String itemName = nameInput.getText();
+        int itemNum = Integer.parseInt(numOfItemInput.getText());
+//            String itemType = in.getItem_type();
+        System.out.println(">>" + codeInput.getText());
+
+//        inventoryController.updateInventory();
+    }//GEN-LAST:event_bntUpdateActionPerformed
+
+    private void nameInputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nameInputActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_nameInputActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton bntUpdate;
+    private javax.swing.JButton btnDelete;
     private javax.swing.JTextField codeInput;
+    private javax.swing.JTextField idInput;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
-    private javax.swing.JButton jButton4;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
@@ -279,6 +325,6 @@ public class AllData extends javax.swing.JFrame {
     private com.toedter.calendar.JDateChooser stackedDate;
     private java.awt.TextField textField1;
     private java.awt.TextField textField2;
-    private javax.swing.JTextField typeInput;
+    private javax.swing.JComboBox<String> typeInput;
     // End of variables declaration//GEN-END:variables
 }
